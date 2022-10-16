8dbba6f9f37967c95d62d992028c5bcd8cfcd521
Revathi G <revathikrishna0323@gmail.com>
2022-10-16 16:38:05 +0530

    written code in AgeVerifierMain.java

--- src/main/java/com/io/github/AgeVerifierMain.java
+++ src/main/java/com/io/github/AgeVerifierMain.java
@@ -3,5 +3,12 @@ package com.io.github;
 public class AgeVerifierMain {
     public static void main(String[] args) {
         AgeVerifier ageVerifier = new AgeVerifier();
+        try {
+            ageVerifier.verifyAge(12);
+        } catch (InvalidAgeException exception) {
+            exception.getMessage();
+            System.out.println(exception.getMessage());
+        }
     }
 }
+
